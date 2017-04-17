15e47212ad8da4762ee8f279bea78ca62ccaa771
jhomble <jhomble7@gmail.com>
2017-04-17 00:14:46 -0400

    fixed param page

--- scripts/app.js
+++ scripts/app.js
@@ -56,98 +56,96 @@
         })
 
         .controller('homeController', ['$scope', '$window', function ($scope, $window) {
-
-            // var jumboHeight = $('.jumbotron').outerHeight();
-            // function parallax() {
-            //     var scrolled = $(window).scrollTop();
-            //     $('.bg').css('height', (jumboHeight - scrolled) + 'px');
-            // }
-
-            // $(window).scroll(function (e) {
-            //     parallax();
-            // });
-
-            $scope.inputTask = true;
-            $scope.inputSmile = false;
-            $scope.inputCauses = false;
-            $scope.builder = false;
-            $scope.relationship = false;
-            $scope.parameters = false;
-
-            var reset = function () {
-                $scope.inputTask = false;
+            $(document).ready(function () {
+                $scope.inputTask = true;
                 $scope.inputSmile = false;
                 $scope.inputCauses = false;
                 $scope.builder = false;
                 $scope.relationship = false;
                 $scope.parameters = false;
-            }
-            $scope.showInputTask = function () {
-                reset();
-                $scope.inputTask = true;
-            }
 
-            $scope.showInputSmile = function () {
-                reset();
-                $scope.inputSmile = true;
-            }
-
-            $scope.showInputCauses = function () {
-                reset();
-                $scope.inputCauses = true;
-            }
-
-            $scope.showBuilder = function () {
-                reset();
-                $scope.builder = true;
-            }
-
-            $scope.showRelationship = function () {
-                reset();
-                $scope.relationship = true;
-            }
-
-            $scope.showParameters = function () {
-                reset()
-                $scope.parameters = true;
-            }
-            // For Actions / Causes
-
-            $scope.knowledge = [
-                {
-                    cause: "Enter Cause",
-                    parameters: [],
-                    actions: []
+                var reset = function () {
+                    $scope.inputTask = false;
+                    $scope.inputSmile = false;
+                    $scope.inputCauses = false;
+                    $scope.builder = false;
+                    $scope.relationship = false;
+                    $scope.parameters = false;
                 }
-            ]
-
-            $scope.addCause = function () {
-                var item = {
-                    cause: "Enter Action",
-                    parameters: [],
-                    actions: []
+                $scope.showInputTask = function () {
+                    reset();
+                    $scope.inputTask = true;
                 }
-                $scope.knowledge.push(item);
-            }
 
-            $scope.removeCause = function () {
-                $scope.knowledge.pop();
-            }
+                $scope.showInputSmile = function () {
+                    reset();
+                    $scope.inputSmile = true;
+                }
 
-            $scope.addAction = function (index) {
-                $scope.knowledge[index].actions.push({ action: "Enter Action", relationship: "Direct" })
-            }
+                $scope.showInputCauses = function () {
+                    reset();
+                    $scope.inputCauses = true;
+                }
 
-            $scope.removeAction = function (index) {
-                $scope.knowledge[index].actions.pop()
-            }
+                $scope.showBuilder = function () {
+                    reset();
+                    $scope.builder = true;
+                }
 
-            $scope.addParameter = function (index) {
-                $scope.knowledge[index].parameters.push({ type: "", value: "" })
-            }
-            
-            $scope.removeParameter = function (index) {
-                $scope.knowledge[index].parameters.pop()
-            }
+                $scope.showRelationship = function () {
+                    reset();
+                    $scope.relationship = true;
+                }
+
+                $scope.showParameters = function () {
+                    reset()
+                    $scope.parameters = true;
+                }
+                // For Actions / Causes
+
+                $scope.knowledge = [
+                    {
+                        cause: "Enter Cause",
+                        relationship: {},
+                        parameters: [],
+                        actions: []
+                    }
+                ]
+
+                $scope.addCause = function () {
+                    var item = {
+                        cause: "Enter Cause",
+                        relationship: {},                        
+                        parameters: [],
+                        actions: []
+                    }
+                    $scope.knowledge.push(item);
+                }
+
+                $scope.removeCause = function () {
+                    $scope.knowledge.pop();
+                }
+
+                $scope.addAction = function (index) {
+                    $scope.knowledge[index].actions.push({ action: "Enter Action", params: []})
+                }
+
+                $scope.removeAction = function (index) {
+                    $scope.knowledge[index].actions.pop()
+                }
+
+                $scope.addActionParameter = function (parentIndex, index) {
+                    $scope.knowledge[parentIndex].actions[index].params.push({ type: "", value: "" })
+                    console.log($scope.knowledge)
+                }
+
+                $scope.removeActionParameter = function (parentIndex, index) {
+                    $scope.knowledge[parentIndex].actions[index].params.pop()
+                }
+                
+                $scope.defaultActions = ['release', 'grab', 'move']
+
+            })
+
         }]);
 })();

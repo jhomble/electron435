a0b142facb93badeafbbbb0c8eb22f5d40f4df93
Ben Mariano <benmariano@wireless-10-104-238-96.umd.edu>
2017-04-25 23:43:28 -0400

    added beginnings of imitation compiler

--- python_causal_compiler/compiler.py
+++ python_causal_compiler/compiler.py
@@ -1214,14 +1214,38 @@ class Facility_Domain_Compiler(NodeVisitor):
 #	Imitation Compiler
 ################################################################
 
+## Imitation Compiler
+#
+#  Compiles the second of the two required python scripts. This 
+#  script traverses the CO-PCT tree in reverse using PyHop.
 class Imitation_Compiler(NodeVisitor):
 
+	## Constructor
 	def __init__(self, parser):
+		## @var parser
+		#  Converts input into AST
 		self.parser = parser
+		## @var methods_dict
+		#  Dictionary of methods where key is method name and
+		#  value is a 3-tuple (list of arguments, cond, returns)
+		self.methods_dict = {}
 
+	## Visit Literal
+	#
+	# Returns a tuple of the string 'LITERAL' and the literal
+	# value
+	#
+	# @rtype: (String, String)
 	def visit_Literal(self, node):
 		return 'LITERAL', str(self.visit(node.name))
 
+	## Visit Boolean
+	#
+	# Returns a three-tuple of the form e1, comp, e2 where
+	# e1 and e2 are tuples representing eithing literals, variables
+	# or keyword phrases
+	#
+	# @rtype: (Tuple, String, Tuple)
 	def visit_Boolean(self, node):
 		if node.op.type == EQUALS :
 			return self.visit(node.e1), "==", self.visit(node.e2)
@@ -1234,12 +1258,22 @@ class Imitation_Compiler(NodeVisitor):
 		elif node.op.type == LESSEQUAL :
 			return self.visit(node.e1), "<=", self.visit(node.e2)
 
+	## Visit Boolean Expression
+	#
+	# Returns a three tuple of the form b_left, op, b_right
+	#
+	# @rtype: (Tuple, String, Tuple)
 	def visit_BoolExpr(self, node):
 		if node.op.type == AND:
 			return self.visit(node.left), "&&", self.visit(node.right)
 		elif node.op.type == OR:
 			return self.visit(node.left) , "||", self.visit(node.right)
 
+	## Visit Arguments
+	#
+	# Returns a list of strings representing the arguments
+	#
+	# @rtype: String List
 	def visit_Args(self, node):
 		args = []
 		for child in node.children:
@@ -1247,9 +1281,19 @@ class Imitation_Compiler(NodeVisitor):
 
 		return args
 
+	## Visit Action
+	#
+	# Returns a tuple of the action_name, action_args
+	#
+	# @rtype: (String, String List)
 	def visit_Act(self, node):
 		return (self.visit(node.var), self.visit(node.args))
 
+	## Visit Actions
+	#
+	# Returns a list of strings representing the actions
+	#
+	# @rtype: (String, String List) List
 	def visit_Acts(self, node):
 		acts = []
 		for child in node.children:
@@ -1257,150 +1301,250 @@ class Imitation_Compiler(NodeVisitor):
 
 		return acts
 
-	def create_Action_Arg_Index_Reference_Dict(acts):
+	## Create Action Argument Index Reference Dictionary
+	#
+	#  Returns a dictionary where the keys are arguments to the 
+	#  actions in a given causal statement and the values are
+	#  the associated indices i,j in the 2d arguments array
+	#
+	# @rtype: {String, (String, String)}
+	def create_Action_Arg_Index_Reference_Dict(self, acts):
 		arg_index_dict = {}
 
 		for i in range(0, len(acts)):
-			for j in range(0, len(acts[i])):
-				arg_index_dict[acts[i][1][j]] = i,j
+			for j in range(0, len(acts[i][1])):
+				# CONT keyword handled later
+				if acts[i][1][j][:4] != 'CONT':
+					arg_index_dict[acts[i][1][j]] = str(i),str(j)
 
 		return arg_index_dict 
 
+	## Visit Caus
+	#
+	# Returns the name of the intention
+	#
+	# @rtype: String
 	def visit_Caus(self, node):
+		# acts = the right-side of the causal statement. Represent
+		#		 the actions that cause the 'intention' 
+		# act  = the left-side of the causal statement. Represents
+		#		 the 'intention' caused by the actions
 		acts = self.visit(node.acts)
 		act = self.visit(node.act)
 
+		# defines fold-left function
 		foldl = lambda func, acc, xs: functools.reduce(func, xs, acc)
 
+		# isolates and formats the names of the acts in order to be
+		# used in the if statement
 		act_names = foldl(operator.add, '', map(lambda x: '\''+x[0]+'\',', acts))
-
-		if_stmt = 'if actions == (' + act_names + '):\n'		
 
 		intention_Args = act[1]
 
-		arg_indices = create_Action_Arg_Index_Reference_Dict(acts)
+		act_name = act[0]
 
-		# arg_indices = []
+		print('ACT: ' + act_name)
 
-		# for arg in intention_Args:
-		# 	if arg[:4] != 'CONT':
-		# 		for i in range(0, len(acts)):
-		# 			j = indexof(acts[i][1], arg)
-		# 			if j >= 0:
-		# 				arg_indices[arg] = (i,j)
-		# 				break
-		# 	else:
-		# 		if len(arg_indices) == 0:
-		# 			raise Exception('Cont used improperly!\n')
-		# 		arg_indices[arg] = arg
+		# return statement
+		ret = 'return ['
 
-		# if len(arg_indices) != len(intention_Args):
-		# 	raise Exception('Args improperly compiled!')
+		# iterate through each action adding it and its 
+		# arguments to the return string
+		for action in acts:
+			ret += '(\''+action[0]+'\','
+			for arg in action[1]:
+				ret += arg + ','
+			ret = ret[:len(ret)-1] + ')'
+			ret += ','
 
-		# args = ''
+		ret = ret[:len(ret)-1] + ']\n'
 
-		# for a in range(0, len(arg_indices)):
-		# 	index = arg_indices[a]
-		# 	if index[:4] != 'CONT':
-		# 		i = str(index[0])
-		# 		j = str(index[1])
-		# 		if (a == len(arg_indices)-1):
-		# 			args += '(arguments['+i+']['+j+'], )' 
-		# 		else:
-		# 			args += '(arguments['+i+']['+j+'], )+' 
-		# 	else:
-		# 		prev_index = arg_indices[a-1]
-		# 		i = str(prev_index[0])
-		# 		j = str(prev_index[1]+1)
-		# 		args += 'arguments['+i+']['+j+':]'
+		print('RET: ' + ret)		
 
-		# args += ')'
+		# Check if method has already been defined
+		if self.methods_dict.has_key(act_name):
+			args, _, _ = self.methods_dict[act_name]
+			# Check if you have to change parameters (add final *)
+			if len(intention_Args) > len(args):
+				'TODO: Handle adjustment of args'
+			self.methods_dict[act_name][2].append(ret)
+		else:
+			self.methods_dict[act_name] = (intention_Args, [], [ret])
 
-		for i in range(0, len(intention_Args)):
-			arg = intention_Args[i] 
-			if arg_indices.hasKey(arg):
-				if len(arg_indices[arg]) == 2:
-					i,j = arg_indices[arg]
-					if (a == len(arg_indices)-1):
-						args += '(arguments['+i+']['+j+'], )' 
-					else:
-						args += '(arguments['+i+']['+j+'], )+' 
-				else:
-					'TODO: HANDLE CONT'
-		gadd = 'g.add((states[0],\''+act[0]+'\','+args+'))\n'
+		return act_name
 
-		print('ARGS: ', args)
-
-		return (if_stmt, gadd)
-
+	## Visit No Conditional
+	#
+	# Return None when there is no conditional
 	def visit_NoCond(self,node):
 		return None
 
+	## Visit Conditional
+	#
+	#  Return the result of evaluating the boolean expression
 	def visit_Cond(self, node):
 		return self.visit(node.boolean)
 
-	def compile_bool(self, cond):
+	## Compile Boolean Statement
+	#
+	# Returns a tuple (body, if statement) that represents the 
+	# required additions to the output to succesffuly match the 
+	# conditional.
+	#
+	# @rtype: (String, String)
+	def compile_bool(self, cond, arg_indices):
+		# body     = Additional things added to the body of if_stmt.
+		#			 This could include calls to lookup_type or
+		#			 defining local variables
+		# if_stmt  = Handles conditional relationship rules. For
+		#			 example, this if statement would include
+		#   		 checking the type of an object		
 		body = ''
 		if_stmt = ''
 
-		print('COND: ', str(cond))
+		return '', ''
 
+		# expr1 = left side of comparison. Could be a variable,
+		# 		  literal, or keyword phrase like TYPE(obj)
+		# comp  = comparison operator (should be '==')
+		# expr2 = right side of comparison. Could be a variable,
+		#		  literal, or keyword phrase like ALL(type)
 		expr1 = cond[0]
 		comp = cond[1]
 		expr2 = cond[2]
 
 		if comp == '==':
 			if expr1[0] == 'ALL':
-				print('ALL')
-
+				# make the body statement develop a list of all objects of type
+				# expr1[1] in the current state (state[0])
+				body += 'all_'+expr1[1]+' = [obj_id for (obj_id, obj_type,_,_,_,_)'
+				body += ' in states[0] if obj_type == \''+expr1[1]+'\']\n'
+				# find argument indices for the left-side of the comparison so
+				# that they can be appropriately referenced in the if_stmt
+				args = ''
+				for a in range(0, len(expr2)):
+					arg = expr2[a]
+					# Handle the special case of Keyword CONT
+					if arg[:4] == 'CONT':
+						prev_arg = expr2[a-1]
+						i,j = arg_indices[prev_arg]
+						args = args[:len(args)-(18+len(i)+len(j))] + 'arguments['+i+']['+j+':]'
+					else:
+						i,j = arg_indices[arg]
+						# don't add + sign if it is the last argument
+						if (a == len(expr2) - 1):
+							args += '(arguments['+i+']['+j+'], )'
+						else:
+							args += '(arguments['+i+']['+j+'], )+'
+				if_stmt += 'if set(all_'+expr1[1]+') == set('+args+'):\n'
 			elif expr1[0] == 'TYPE':
-				print('TYPE')
 				var_name = str(expr1[1])
-				body += var_name+'_type = lookup_type('+var_name+', states[0])\n'
+				i,j = arg_indices[var_name]
+				var_get = 'arguments['+i+']'+'['+j+']'
+				body += var_name+'_type = lookup_type('+var_get+', states[0])\n'
 				if_stmt += 'if '+var_name+'_type == \''+str(expr2)+'\':\n'
 			else:
-				'Var1 = Var2 or Var1 = Literal'
+				# var1 and var2 could be either variables or literals
+				var1 = ''
+				var2 = ''
+				if expr1[0] == 'LITERAL':
+					var1 = '\''+str(expr1[1])+'\''
+				else:
+					i,j = arg_indices[expr1]
+					var1 = 'arguments['+i+']['+j+']'
+
+				if expr2[0] == 'LITERAL':
+					var2 = '\''+str(expr2[1])+'\''
+				else:
+					i,j = arg_indices[expr2]
+					var2 = 'arguments['+i+']['+j+']'
+				if_stmt += 'if '+var1+' == '+var2+':\n'
 		else:
 			raise Exception('\''+str(comp)+'\' currently not supported')
 
 		return body, if_stmt
 
+	## Visit Statement
+	#
+	# Evalulates a causal relation and a conditional. Returns None.
+	#
+	# @rtype: None
 	def visit_Stmt(self, node):
-		if_stmt, gadd = self.visit(node.caus)
+		# if_stmt 	  = initial if statement string that differentiates
+		#				which rule is being applied
+		# gadd    	  = g.add statement string that adds the result of the 
+		#				rule to the final set
+		# arg_indices = dictionary containing the i,j indices in the 2d
+		#				arguments array for each of the arguments of the
+		#				actions
+		intention = self.visit(node.caus)
+
+		# cond = tuple representing the conditions under which the rule
+		#		 holds. See visit_BoolExpr for more insight into the 
+		#		 formatting here
 		cond = self.visit(node.cond)
 
-		comps = ['==', '<', '>', '<=', '>=']
+		self.methods_dict[intention][1].append(cond)
 
-		if cond:
-			if cond[1] in comps:
-				print('Single: '+str(cond))
-				print(self.compile_bool(cond))
-			else:
-				print('Multiple: '+str(cond))
-				for i in range(0, len(cond)):
-					if cond[i] == '&&':
-						'TODO: Mess with indenting'
-					elif cond[i] == '||':
-						'TODO: Mess with indenting'
-					else:
-						print(self.compile_bool(cond[i]))
+		return None
 
-
-		return ''
-
+	## Visit Statements
+	#
+	# Compile all statements and concatenate results
+	#
+	# @rtype: String
 	def visit_Stmts(self, node):
 		result = ''
 		for child in node.children:
-			result += self.visit(child)
+			self.visit(child)
+
+		tab = '    '
+
+		print('DICT:')
+		for k in self.methods_dict:
+			v = self.methods_dict[k]
+			print(str(k) + ': ' + str(v))
+
+		for intention in self.methods_dict:
+			args = self.methods_dict[intention][0]
+			rets = self.methods_dict[intention][2]
+			method_dec = 'def '
+			method_dec += intention
+			method_dec += '(state'
+			for arg in args:
+				method_dec += ', '+arg
+			method_dec += '):\n'
+			pyhop_stmt = 'pyhop.declare_methods(\''+intention+'\','
+			pyhop_stmt += intention+')\n'
+			result += method_dec
+			for ret in rets:
+				result += tab + ret
+			result += pyhop_stmt
 
 		return result
 
+	## Visit Variable
+	#
+	# Return a string representing the variable value/name
+	# 
+	# @rtype: String
 	def visit_Var(self, node):
 		return str(node.value)
 
+	## Visit Digit
+	#
+	# Returns a string representing a digit
+	#
+	# @rtype: String
 	def visit_Digit(self, node):
 		return str(node.value)
 
+	## Visit Integer
+	#
+	# Returns a string representing a full integer, which is a 
+	# string of concatenated digits
+	#
+	# @rtype: String
 	def visit_Int(self, node):
 		result = ''
 
@@ -1409,18 +1553,45 @@ class Imitation_Compiler(NodeVisitor):
 
 		return result
 
+	## Visit Float
+	#
+	# Returns a float string which is two integers separated by
+	# a dot
+	#
+	# @rtype: String
 	def visit_Flt(self, node):
 		return self.visit(node.left) + '.' + self.visit(node.right)
 
+	## Visit ALL
+	#
+	# Returns a tuple of the form ('All', argument)
+	#
+	# @rtype: (String, String)
 	def visit_All(self, node):
 		return ALL, self.visit(node.arg)
 
+	## Visit Type
+	#
+	# Returns a tuple of the form ('TYPE', argument)
+	#
+	# @rtype: (String, String)
 	def visit_Type(self, node):
 		return TYPE, self.visit(node.arg)		
 
+	## Visit NoOp
+	#
+	# Returns the empty string
+	#
+	# @rtype: String
 	def visit_NoOp(self, node):
 		return ''
 
+	## Interpret
+	#
+	# Actually compile the statement. Returns a tuple of the string 
+	# representing the compiled program as well as the value of M
+	#
+	# @rtype: (String, String)
 	def interpret(self):
 		tree = self.parser.parse()
 		return self.visit(tree)
@@ -1466,11 +1637,20 @@ def make_imitation(interpreter):
 def main():
 	text = open(sys.argv[1], 'r').read()
 
-	lexer = Lexer(text)
-	parser = Parser(lexer)
-	facility_interpreter = Facility_Domain_Compiler(parser)
-	facility_domain_result = make_facility_domain(facility_interpreter)
-	#imitation_result = make_imitation(interpreter)
+	makeFacility = False;
+	makeImitation = True;
+
+	if makeFacility:
+		facility_lexer = Lexer(text)
+		facility_parser = Parser(facility_lexer)
+		facility_interpreter = Facility_Domain_Compiler(facility_parser)
+		facility_domain_result = make_facility_domain(facility_interpreter)
+
+	if makeImitation:
+		imitation_lexer = Lexer(text)
+		imitation_parser = Parser(imitation_lexer)
+		imitation_interpreter = Imitation_Compiler(imitation_parser)
+		imitation_result = make_imitation(imitation_interpreter)
 
 ## Index Of
 # 

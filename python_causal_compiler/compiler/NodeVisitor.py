68afa9c574e66920ed7e0e1caab349584130c81f
Ben Mariano <benmariano@Bens-MacBook-Pro.local>
2017-05-09 17:32:51 -0400

    expanded comments and reformatted for doxygen

--- python_causal_compiler/compiler/NodeVisitor.py
+++ python_causal_compiler/compiler/NodeVisitor.py
@@ -1,10 +1,17 @@
 ## Node Visitor
 #
+#  @filename NodeVisitor.py
+#  @author Ben Mariano
+#  @date 5/9/2017
+#
 #  Visits all nodes of the AST by virtue of the visit_* methods
 class NodeVisitor(object):
 	## Visit
 	#
-	# visits the AST node by calling the visit_'node' function
+	# @brief visits the AST node by calling the visit_'node' function
+	#
+	# @param node AST class to be evaluated
+	# @retval object some object representation of the node
 	def visit(self, node):
 		method_name = 'visit_'+type(node).__name__
 		visitor = getattr(self, method_name, self.generic_visit)
@@ -12,6 +19,8 @@ class NodeVisitor(object):
 
 	## Visit Error
 	#
-	# Alerts user if AST node has no defined visit function
+	# @brief Alerts user if AST node has no defined visit function
+	#
+	# @param node AST class to be evaluated	
 	def generic_visit(self, node):
 		raise Exception('No visit_{} method'.format(type(node).__name__))
